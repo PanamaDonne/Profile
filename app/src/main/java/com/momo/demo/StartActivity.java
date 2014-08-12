0cd69f0ec1469c2c1f23a78aa8f4f7c4f962238f
PanamaDonne <createdbydaniel@gmail.com>
2014-08-12 22:14:30 +0200

    Condo & School Activities

--- app/src/main/java/com/momo/demo/StartActivity.java
+++ app/src/main/java/com/momo/demo/StartActivity.java
@@ -1,11 +1,17 @@
 package com.momo.demo;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,23 +22,43 @@ public class StartActivity extends Activity {
     private ListView listView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_start);
 
-        listView = (ListView) findViewById(R.id.listView);
+        try {
+            setContentView(R.layout.activity_start);
 
-        List<String> app_list = new ArrayList<String>();
-        app_list.add("School Application");
-        app_list.add("Condo Application");
+            listView = (ListView) findViewById(R.id.listView);
+            listView.setAdapter(new ArrayAdapter<String>(this,
+                    android.R.layout.simple_list_item_1,
+                    new String[] {"School Application", "Condo Application"}));
 
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
-                this,
-                android.R.layout.simple_list_item_1,
-                app_list );
+            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                public void onItemClick(AdapterView<?> parent, View view,
+                                        int position, long id) {
+                    String sText = ((TextView) view).getText().toString();
+                    Intent intent = null;
+                    if(sText.equals("School Application")) {
+                        intent = new Intent(getBaseContext(),
+                                CondoActivity.class);
+                    }
+                    else if(sText.equals("Condo Application")) {
+                        intent = new Intent(getBaseContext(),
+                                CondoActivity.class);
+                    }
 
-        listView.setAdapter(arrayAdapter);
+                    if(intent != null)
+                        startActivity(intent);
+                }
+            });
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
     }
+
+
 
 
     @Override
