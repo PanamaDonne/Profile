5c212952ce751fdfa08f82304f2970f7817fe962
PanamaDonne <createdbydaniel@gmail.com>
2014-08-13 23:06:45 +0200

    Added Condo button grid

--- app/src/main/java/com/momo/demo/CondoActivity.java
+++ app/src/main/java/com/momo/demo/CondoActivity.java
@@ -4,33 +4,33 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 
 public class CondoActivity extends Activity {
+
+    private Button button1;
+    private Button button2;
+    private Button button3;
+    private Button button4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_condo);
+
+        button1=(Button)findViewById(R.id.button1);
+        button1.setText("SCHEDULE");
+
+        button2=(Button)findViewById(R.id.button2);
+        button2.setText("CLASSES");
+
+        button3=(Button)findViewById(R.id.button3);
+        button3.setText("NEWS");
+
+        button4=(Button)findViewById(R.id.button4);
+        button4.setText("REPORT");
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.condo, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
