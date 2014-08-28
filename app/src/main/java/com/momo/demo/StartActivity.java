1328a735ddb7ac93b18ad56e24d2a05a0569d7c0
PanamaDonne <createdbydaniel@gmail.com>
2014-08-28 21:38:13 +0200

    Added Splash Screen

--- app/src/main/java/com/momo/demo/StartActivity.java
+++ app/src/main/java/com/momo/demo/StartActivity.java
@@ -2,81 +2,47 @@ package com.momo.demo;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.List;
 
 
 public class StartActivity extends Activity {
-
-    private ListView listView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        try {
-            setContentView(R.layout.activity_start);
+        setContentView(R.layout.activity_start);
 
-            listView = (ListView) findViewById(R.id.listView);
-            listView.setAdapter(new ArrayAdapter<String>(this,
-                    android.R.layout.simple_list_item_1,
-                    new String[] {"School Application", "Condo Application"}));
+        startTimer();
 
-            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                public void onItemClick(AdapterView<?> parent, View view,
-                                        int position, long id) {
-                    String sText = ((TextView) view).getText().toString();
-                    Intent intent = null;
-                    if(sText.equals("School Application")) {
-                        intent = new Intent(getBaseContext(),
-                                CondoActivity.class);
-                    }
-                    else if(sText.equals("Condo Application")) {
-                        intent = new Intent(getBaseContext(),
-                                CondoActivity.class);
-                    }
+    }
 
-                    if(intent != null)
-                        startActivity(intent);
+
+
+    /*
+     ** Set timer to splash
+     */
+    void startTimer() {
+        Thread timer=new Thread()
+        {
+            public void run() {
+                try {
+                    sleep(3000);
+                } catch (InterruptedException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
                 }
-            });
-        } catch (Exception e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-
+                finally
+                {
+                    Intent i=new Intent(StartActivity.this,CondoActivity.class);
+                    finish();
+                    startActivity(i);
+                }
+            }
+        };
+        timer.start();
     }
 
 
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.start, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
