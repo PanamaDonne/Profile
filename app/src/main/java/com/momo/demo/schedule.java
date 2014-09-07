00b841912af4d66fa79a20818f7b6b01570f3255
PanamaDonne <createdbydaniel@gmail.com>
2014-09-07 22:50:51 +0200

    textview added + parsing jsonarray

--- app/src/main/java/com/momo/demo/schedule.java
+++ app/src/main/java/com/momo/demo/schedule.java
@@ -11,7 +11,9 @@ import android.widget.CalendarView;
 import android.widget.ListView;
 
 import java.lang.reflect.Array;
+import java.text.DateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import com.parse.FindCallback;
@@ -25,6 +27,7 @@ import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -42,13 +45,14 @@ public class schedule extends Activity {
 
     private String TAG;
     private ListView listView;
-    ParseQuery<ParseObject> query;
     public String date;
-    public ArrayList <String> list;
-    ParseQueryAdapter adapter;
     private Boolean booked;
-
+    private String period;
+    private ArrayList<String> listItems=new ArrayList<String>();
+    private ArrayAdapter<String> adapter;
+    private String ObjectId;
     private CalendarView calendarView;
+    String currentDateTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,21 +60,36 @@ public class schedule extends Activity {
         TAG = "calendarActivity";
         setContentView(R.layout.activity_schedule);
         calendarView = (CalendarView) findViewById(R.id.calendarView);
+        listView = (ListView) findViewById(R.id.listView);
 
         // INIT PARSE.COM
         parse();
 
-        adapter = new ParseQueryAdapter(this, "booking_tennis");
-        adapter.setTextKey("periods");
+         currentDateTime = DateFormat.getDateTimeInstance().format(new Date());
 
-        final ListView listView = (ListView) findViewById(R.id.listView);
+        // textView is the TextView view that should display it
+        TextView text = (TextView) findViewById(R.id.textView);
+        text.setText(currentDateTime);
+
+        listItems.add("07:00 - 08:00");
+        listItems.add("08:00 - 09:00");
+
+        adapter=new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                listItems);
         listView.setAdapter(adapter);
+
+
+
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View view,
                                     int position, long id) {
 
-                final String ObjectId = adapter.getItem(position).getObjectId();
+
+                Log.i(TAG, "DATE LIST CLICK: " + date);
+
+                //PARSEQUERY STARTS WITH "DATE"
 
                 AlertDialog.Builder builder1 = new AlertDialog.Builder(schedule.this);
                 builder1.setMessage("Você quer agendar este horario?");
@@ -79,15 +98,15 @@ public class schedule extends Activity {
                         new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog, int id) {
 
-                                ParseQuery<ParseObject> query = ParseQuery.getQuery("booking_tennis");
+                                ParseQuery<ParseObject> query = ParseQuery.getQuery("booking_test");
 
 
                                 query.getInBackground(ObjectId, new GetCallback<ParseObject>() {
-                                    public void done(ParseObject booking_tennis, ParseException e) {
+                                    public void done(ParseObject booking_test, ParseException e) {
                                         if (e == null) {
 
-                                            booking_tennis.put("booked", true);
-                                            booking_tennis.saveInBackground();
+                                            booking_test.put("booked", true);
+                                            booking_test.saveInBackground();
                                             booked = true;
 
 
@@ -123,15 +142,21 @@ public class schedule extends Activity {
 
 
 
+
+
+
         calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(CalendarView view, int year, int month,
                                             int dayOfMonth) {
 
                 StringBuilder sb = new StringBuilder();
+                sb.append(year);
+                sb.append("0" + month);
                 sb.append(dayOfMonth);
                 date = sb.toString();
                 Log.i(TAG, "DATE: " + date);
+
 
 
 
@@ -145,9 +170,50 @@ public class schedule extends Activity {
     }
 
 
+
+
     void parse() {
         Parse.initialize(this, "fNj6swlEg1d5Rn4rO8jBPwJ6BlAbDN0A2GJbYnTB", "6Ua0deolkpYrnWagJRZcoRulDI2BHbLFccXzW85E");
 
+    }
+
+
+    void parseQuery() {
+
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("booking_test");
+
+
+
+        query.getInBackground("p3v9ix1D94", new GetCallback<ParseObject>() {
+            public void done(ParseObject booking_test, ParseException e) {
+                if (e == null) {
+
+                    JSONArray periodArr = booking_test.getJSONArray("periods");
+
+                    Log.i(TAG, "JSONARR: " + periodArr);
+
+                    for(int i=0; i< periodArr.length(); i++){
+
+                        period = null;
+                        try {
+                            period = periodArr.getString(i);
+                        } catch (JSONException e1) {
+                            e1.printStackTrace();
+                        }
+
+                        Log.i(TAG, "STRING: " + period);
+
+                        listItems.add(period);
+
+                    }
+
+                }
+            }
+        });
+
+
+
+        return;
     }
 
 
@@ -177,24 +243,6 @@ public class schedule extends Activity {
     }
 
 
-    void bookPeriod() {
-        ParseQuery<ParseObject> query = ParseQuery.getQuery("booking_tennis");
-
-        // Retrieve the object by id
-        query.getInBackground("xWMyZ4YEGZ", new GetCallback<ParseObject>() {
-            public void done(ParseObject gameScore, ParseException e) {
-                if (e == null) {
-                    // Now let's update it with some new data. In this case, only cheatMode and score
-                    // will get sent to the Parse Cloud. playerName hasn't changed.
-                    String objectId = gameScore.getObjectId();
-                    gameScore.put("score", 1338);
-                    gameScore.put("cheatMode", true);
-                    gameScore.saveInBackground();
-                }
-            }
-        });
-
-    }
 
 
 
