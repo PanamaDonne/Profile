5f377ca4047e8de918a5165d397eb53b4f41b930
PanamaDonne <createdbydaniel@gmail.com>
2014-11-15 13:46:23 +0100

    Delete student fucntion

--- app/src/main/java/com/momo/profile/StudentDetail.java
+++ app/src/main/java/com/momo/profile/StudentDetail.java
@@ -1,12 +1,17 @@
 package com.momo.profile;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.parse.DeleteCallback;
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
@@ -19,24 +24,36 @@ import java.util.List;
 public class StudentDetail extends Activity {
 
     private TextView student;
+    private TextView teacher;
     private TextView day1;
     private TextView day2;
     private TextView day3;
     private TextView time1;
     private TextView time2;
     private TextView time3;
+    private Button deleteStudentBtn;
+    private Button deleteTeacherBtn;
     private String weekDay;
     private String TAG;
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        TAG = "StudentDetail";
+
+
+        context = this;
 
 
         setContentView(R.layout.activity_student_detail);
 
         student = (TextView)findViewById(R.id.studentName);
         student.setText((getIntent().getStringExtra("studentName")));
+
+        teacher = (TextView)findViewById(R.id.teacherName);
+
 
         day1 = (TextView)findViewById(R.id.day1);
         day2 = (TextView)findViewById(R.id.day2);
@@ -47,11 +64,33 @@ public class StudentDetail extends Activity {
         time3 = (TextView)findViewById(R.id.time3);
 
 
+        deleteStudentBtn = (Button) findViewById(R.id.btnDeleteStudent);
+        deleteTeacherBtn = (Button) findViewById(R.id.btnDeleteStudent);
+
+
         parseClasses();
 
+        // STUDENT & TEACHER BUTTONS CLICK EVENTS
+        deleteStudentBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                Log.i(TAG, "PRESSED");
 
 
-        TAG = "StudentDetail";
+            }
+
+        });
+
+        deleteTeacherBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                Log.i(TAG, "PRESSED");
+                deleteStudent();
+
+
+            }
+
+        });
 
 
     }
@@ -71,6 +110,7 @@ public class StudentDetail extends Activity {
 
                     day1.setText(classList.get(0).getString("weekDay").toUpperCase());
                     time1.setText(classList.get(0).getString("time"));
+                    teacher.setText("( " + classList.get(0).getString("teacher") + " )");
 
                     if(classList.size() > 1) {
                         day2.setText(classList.get(1).getString("weekDay").toUpperCase());
@@ -90,4 +130,64 @@ public class StudentDetail extends Activity {
             }
         });
     }
+
+
+    void deleteStudent() {
+        final ParseQuery<ParseObject> query = ParseQuery.getQuery("student");
+
+        final String studentString = student.getText().toString();
+
+        query.whereEqualTo("name", studentString);
+
+        query.findInBackground(new FindCallback<ParseObject>() {
+            public void done(List<ParseObject> classList, ParseException e) {
+                if (e == null && classList.size() > 0) {
+
+                    classList.get(0).deleteInBackground(new DeleteCallback() {
+                        public void done(ParseException e) {
+                            if (e == null) {
+                                Toast.makeText(context, studentString + " deleted",
+                                        Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(context, "  not deleted. Please try again.",
+                                        Toast.LENGTH_SHORT).show();
+                                e.printStackTrace();
+
+                            }
+                        }
+                    });
+
+                    if(classList.size() > 1) {
+                        classList.get(1).deleteInBackground(new DeleteCallback() {
+                            public void done(ParseException e) {
+                                if (e == null) {
+
+                                } else {
+                                    
+                                    e.printStackTrace();
+
+                                }
+                            }
+                        });
+                    }
+
+                } else {
+
+                    Toast.makeText(context, "  not found. Please try again.",
+                            Toast.LENGTH_SHORT).show();
+                    e.printStackTrace();
+
+
+                }
+
+
+            }
+        });
+
+
+
+    }
+
+
+
 }
